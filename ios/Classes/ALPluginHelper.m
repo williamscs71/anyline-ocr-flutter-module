cd2e1138f048697917d481d3a4ce6d518c67ff75
william <william.mai@scs71.com>
2025-04-15 16:46:50 +0700

    Update ALPluginHelper.m

--- ios/Classes/ALPluginHelper.m
+++ ios/Classes/ALPluginHelper.m
@@ -12,6 +12,13 @@ NSErrorDomain const ALFlutterDomain = @"ALFlutterDomain";
 
 + (void)startScan:(NSDictionary *)config initializationParamsStr:(NSString *)initializationParamsStr finished:(ALPluginCallback)callback {
     
+    // Adding an exception to reproduce crash issue
+    NSException* exception = [NSException
+                              exceptionWithName:@"AnylineCrashTest"
+                              reason:@"Intentional crash to reproduce issue"
+                              userInfo:nil];
+    [exception raise];
+    
     NSDictionary *pluginConf = config;
     
     NSString *licenseKey = [config objectForKey:@"licenseKey"];
